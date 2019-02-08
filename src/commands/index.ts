e588a59a8a9e759cca70343b1b26a6331f2cafcb
Philipp Riegger <philipp.riegger@itemis.com>
2019-02-08 15:16:00 +0100

    handle errors while displaying the help message

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -59,11 +59,19 @@ export async function printHelp(io: IO, error?: Error) {
     } else {
         io.out(HELP_HEADER)
 
-        const clusters = await call(clusterModule.clusters)()
-        io.out('configured clusters: ' + m(clusters.join(', ')))
+        try {
+            const clusters = await call(clusterModule.clusters)()
+            io.out('configured clusters: ' + m(clusters.join(', ')))    
+        } catch(e) {
+            io.error('failed to retrieve configured clusters')
+        }
 
-        const endpoint = await call(imageModule.endpoint)()
-        io.out('configured image endpoint: ' + m(endpoint))
+        try {
+            const endpoint = await call(imageModule.endpoint)()
+            io.out('configured image endpoint: ' + m(endpoint))    
+        } catch(e) {
+            io.error('failed to retrieve configured image endpoint')
+        }
 
         io.out('')
         io.out(chalk.bold.underline('Available Commands'))
